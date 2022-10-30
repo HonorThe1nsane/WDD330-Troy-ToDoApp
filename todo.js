bd7be02d5558236cc141f9b6e962af3903db5f27
HonorThe1nsane <troymd2009@hotmail.com>
2022-10-30 01:11:58 -0600

    update

--- todo.js
+++ todo.js
@@ -2,18 +2,6 @@
 
 class ToDoList {
 
-    // closeButton() {
-    //     var myNodelist = document.getElementsByTagName("LI");
-    //     var i;
-    //     for (i = 0; i < myNodelist.length; i++) {
-    //         var span = document.createElement("SPAN");
-    //         var txt = document.createTextNode("\u00D7");
-    //         span.className = "close";
-            
-    //         span.appendChild(txt);
-    //         myNodelist[i].appendChild(span);
-    //     }
-    // }
 
     hideClosedItem() {
         var list = document.querySelector('ul');
